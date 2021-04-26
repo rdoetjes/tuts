4416ec1050c32359ec1d8f270b4ee311d852db23
Ray Doetjes <rdoetjes@phonax.com>
2021-04-26 14:19:18 +0200

    indent size reduction python

--- fast_odd_even/fast_odd_even.py
+++ fast_odd_even/fast_odd_even.py
@@ -5,16 +5,16 @@ import time
 ITS = 10000000
 
 def slow():
-	for i in range (0, ITS):
-		if i % 2 == 0:
-			pass
+    for i in range (0, ITS):
+        if i % 2 == 0:
+            pass
 
 def fast():
-	for i in range (0, ITS):
-		if i & 2:
-			pass
+    for i in range (0, ITS):
+        if i & 2:
+            pass
 
-
+    
 start = time.time_ns()
 slow()
 stop  = time.time_ns()
