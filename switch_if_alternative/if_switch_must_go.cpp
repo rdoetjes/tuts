9e43ddf862844fcf7708207c48712eec57ac345b
Ray Doetjes <rdoetjes@phonax.com>
2021-05-17 12:57:51 +0200

    Made the C++ assignment match the JavaScript and Python examples

--- switch_if_alternative/if_switch_must_go.cpp
+++ switch_if_alternative/if_switch_must_go.cpp
@@ -15,24 +15,10 @@ int main(){
   std::string choice="";
 
   //declare a map (hash) of 3 function pointers that take int as argument. C++ as std::function which would be a cleaner more c++ approach for prototyping def.
-  std::map<std::string, std::string (*)()> functions;
+  std::map<std::string, std::string (*)()> functions = { {"one", &one}, {"1", &one}, {"two", &two}, {"2", &two}, {"three", &three}, {"3", &three} };
 
-  //fill the map (you'd do that in your own function normally or based on configuration file for example)
-  functions["one"] = &one;
-  functions["1"] = &one;
-  functions["two"] = &two;
-  functions["2"] = &two;
-  functions["three"] = &three;
-  functions["3"] = &three;
- 
   std::cout << "enter: one, two or three" << std::endl;
   std::cin >> choice;
 
   std::cout << ( (functions.find(choice)!=functions.end()) ? functions[choice]() : "Whoops wrong input motherfucker" );  
-
-  //or when you are not a fan of ternary statement, you can do with a single readable if else
-  if (functions.find(choice)!=functions.end())
-    std::cout << functions[choice]() << "\n\n";
-  else
-    std::cout << "Whoops, wrong input motherfucker!";
 }
