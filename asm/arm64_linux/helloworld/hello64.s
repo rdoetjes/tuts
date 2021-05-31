657abdee3148fc36d5c1789de08a9752400bdccf
Ray Doetjes <rdoetjes@phonax.com>
2021-05-31 08:26:45 +0100

    remarks toupper

--- asm/arm64_linux/helloworld/hello64.s
+++ asm/arm64_linux/helloworld/hello64.s
@@ -3,6 +3,7 @@
 .global _start
 
 _start:
+
 	ldr X1, =string
 	ldr X2, =len
 	bl print
@@ -10,12 +11,14 @@ _start:
 	mov X0, #0
 	bl exit
 
+//X1 is string ptr, X2 is string lenmg
 print:
 	mov X0, #1
-	mov X8, #64
+	mov X8, #64	//write syscall 
 	svc #0
 	ret
 
+//X0 contains exit code
 exit:
 	mov X8, #93
 	svc #0
@@ -23,5 +26,5 @@ exit:
 
 .data
 string:
-	.ascii "Hello world, ARM64 is the way to go!\n"
+	.ascii "Hello World, ARM64 rules!\n"
 len = . - string
