8b30f6f11e155df6c0f925158c165d3c1eaaea65
Ray Doetjes <rdoetjes@phonax.com>
2021-06-29 23:12:50 +0100

    all the comments added

--- asm/arm64_linux/bitcount/bitcount.s
+++ asm/arm64_linux/bitcount/bitcount.s
@@ -2,100 +2,109 @@
 .align 8
 .global _start
 
-_start:
-	ldr x1, =s_bitcount
-	ldr x2, =len_s_bitcount
-	bl print
+_start:	
+	ldr x1, =s_bitcount		//set x1 to string pointed to by s_bitcount
+	ldr x2, =len_s_bitcount		//set x2 to the length of the s_bitcount string
+	bl print			//print the string to the screen
 
-	ldr x0, =value
-	bl printNr
+	ldr x0, =value			//set the number that we want to count the number of set bits from
+	bl printNr			//print that number in decimal
 
-	ldr x1, =s_bitcount1
-	ldr x2, =len_s_bitcount1
-	bl print
+	ldr x1, =s_bitcount1		//set x1 to string pointed to by s_bitcount2
+	ldr x2, =len_s_bitcount1        //set x3 to the length of the s_bitcount1 strong
+	bl print			//print the s_bitcount1 string to the screen
 
-	bl bitCount
-	bl printNr
-
+	bl bitCount			//count the number of set bits in X0, result will be in X0
+	bl printNr			//prints the number of set bits
+	
 	mov X0, #0			//set the exit code to 0
 	bl exit				//call exit
 
 //Will count the number of set bits in the number in register X0
 //Result X0 will contain the number of set bits after calling this routine
 bitCount:
-	stp x29, x30, [sp, #-16]!
-	stp x1, x2, [sp, #-16]!
+	stp x29, x30, [sp, #-16]!	//store frame pointer and  stack pointer on the stack
+	stp x1, x2, [sp, #-16]!		//store x1 and x2 on the stack (so they won't be globbered)
 	
-	mov x2, #0
+	mov x2, #0			//x2 is used to count the set bits, initialize it to 0
 bitCount_Counter:
-	cmp x0, #0
-	beq bitCount_Exit
-	add x2, x2, #1
-	sub x1, x0, #1
-	and x0, x0, x1
-	bne bitCount_Counter
+	cmp x0, #0			//if x0 is 0 then there's no more set bits and we should return
+	beq bitCount_Exit		//return when there's no more set bits
+	add x2, x2, #1			//increment the bit counter, everytimne we hit the loop a bit is set
+
+	sub x1, x0, #1			//n &= n-1 this is the Kernighan algorithm, to count bits efficiently
+	and x0, x0, x1			//(where x0 = n and x1 = n-1)
+
+	bne bitCount_Counter		//x0 is not 0 yet, so there are still be set bits in the value x0
 
 bitCount_Exit:
-	mov x0, x2
-	ldp x1, x2, [sp], #16
-	ldp x29, x30, [sp], #16
-	ret
+	mov x0, x2			//copy number of set bits to x0, as x0 contains our result
+	ldp x1, x2, [sp], #16		//pop x1 and x2 from stack (so they won't be globbered)
+	ldp x29, x30, [sp], #16		//pop fp and sp from stack (so they won't be globbered)
+	ret				//return
 
 //Print value in X0 as an unisgned int to screen
 printNr:
-	stp x29, x30, [sp, #-16]!
-	stp x5, x7, [sp, #-16]!
-	stp x2, x3, [sp, #-16]!
-	stp x0, x1, [sp, #-16]!
+	stp x29, x30, [sp, #-16]!	//store frame pointer and  stack pointer on the stack
+	stp x5, x7, [sp, #-16]!		//push x5 and x7 to stack (so they won't be globbered)
+	stp x2, x3, [sp, #-16]!		//push x2 and x3 to stack (so they won't be globbered)
+	stp x0, x1, [sp, #-16]!		//push x0 and x1 to stack (so they won't be globbered)
 
-	mov x7, #10
-	mov x5, #0
-	sub sp, sp, #128
+	mov x7, #10			//x7 will contain the divider (10) used in udiv and msub
+	mov x5, #0			//x5 counts the number of digits stored on stackl
+	sub sp, sp, #128		//move stack pointer down 128 bytes, so we have space to store the to print digits
+	
+	cmp x0, #0			//if x0=0 then the division algorith will not work
+	beq printNr_Zero		//we set the value on the stack to 0
+
 printNr_Count:
-	udiv x2, x0, x7
-	msub x3, x2, x7, x0
-	add x5, x5, #1
-	strb w3, [sp, x5]
-	mov x0, x2
-	cmp x0, #0
-	bne printNr_Count
+	udiv x2, x0, x7			//divide the value x0 by 10
+	msub x3, x2, x7, x0		//obtain the remainder (x3) and the Quotient (x2)
+	add x5, x5, #1			//increment the digit counter (x5)
+	strb w3, [sp, x5]		//store the digit on the stack as single byte
+	mov x0, x2			//copy the Quotient (x2) into x0 which is the new value to divide by 10
+	cmp x0, #0			//if the Quotient (x0) is 00 then we found all individual digits
+	bne printNr_Count		//if x0 is not yet zero than there's more digits to extract
+	b printNr_print			//we set all the digits on the stack now we can pop them off and print them
 
+printNr_Zero:				//this is the exceptional case when x0 is 0 then we need to push this ourselves to the stack
+	add x5, x5, #1 			//x5 is not used so still 0, there for we need to offset it by 1 for the sp offset
+	strb w0, [sp, x5]		//set the value 0 to the stack, so that it can be printed to the screen
+
+	//using the stacl guarantees that the digits are printed in the right order (from large to smallest_
 printNr_print:
-	cmp x5, #00
-	beq printNr_exit
-
-	ldrb w3, [sp, x5]
-	ldr X1,=char
-	add w3, w3, 48
-	strb w3, [x1]
-	mov x2,#1 
-	bl print
-	subs x5, x5, #1
-	bne printNr_print
+	ldrb w3, [sp, x5]		//pop the last digit from the stack (the biggest value)
+	ldr X1,=char			//set X1 to the char variable address, so we can store the char there later on
+	add w3, w3, 48			//add 48 to the number, turning it into an ASCII char 0-9
+	strb w3, [x1]			//store the ASCII char in the char variable (pointed to by X1)
+	mov x2,#1 			//set the length to write() to 1
+	bl print			//call the writer() system call wrapper 
+	subs x5, x5, #1			//reduce x5 by 1, pointing to the next digit on the stack 
+	bne printNr_print		//if x5 is not 0 then there are still digits on the stack, that should be printed
 
 printNr_exit:
-	add sp, sp, #128
-	ldp x0, x1, [sp], #16
-	ldp x2, x3, [sp], #16
-	ldp x5, x7, [sp], #16
-	ldp x29, x30, [sp], #16
-	ret 
+	add sp, sp, #128		//reclaim the 128 bytes local storage on the stack 
+	ldp x0, x1, [sp], #16		//pop x0 and x1 from stack (so they won't be globbered)
+	ldp x2, x3, [sp], #16		//pop x2 and x3 from stack (so they won't be globbered)
+	ldp x5, x7, [sp], #16		//pop x5 and x7 from stack (so they won't be globbered)
+	ldp x29, x30, [sp], #16		//pop fp and sp from stack (so they won't be globbered)
+	ret 				//return
 
 //Print to STDOUT the message pointed by X1
 //X1 is string ptr 
 //X2 is string length of the string
 print:
-	stp x29, x30, [sp, #-16]!
-	stp x2, x8, [sp, #-16]!
-	stp x0, x1, [sp, #-16]!
+	stp x29, x30, [sp, #-16]!	//store fp and sp on the stack
+	stp x2, x8, [sp, #-16]!		//store x2 and x8 on the stack (so they won't be globbered)
+	stp x0, x1, [sp, #-16]!		//store x0 and x1 on the stack (so they won't be globbered)
 
 	mov X0, #1			//set X0 to point to standard out
 	mov X8, #64			//write syscall 
 	svc #0				//call system call (64 => write)
 
-	ldp x0, x1, [sp], #16
-	ldp x2, x8, [sp], #16
-	ldp x29, x30, [sp], #16
+	ldp x0, x1, [sp], #16		//pop x0 and x1 from stack (so they won't be globbered)
+	ldp x2, x8, [sp], #16		//pop x2 and x8 from stack (so they won't be globbered)
+	ldp x29, x30, [sp], #16		//pop fp and sp from stacl
 	ret				//return to caller
 
 //Exit to operating system, X0 will contain the exit code
@@ -103,7 +112,7 @@ print:
 exit:
 	mov X8, #93			//exit system call
 	svc #0				//call system call (93 => exit)
-	ret
+	ret				//this won't be called because exit will terminate program before we get here
 
 .data
 s_bitcount:	.ascii "We are counting the number of set bits in #"
