8ae25ac12fe314950a7558f66834277d4ddb1ac4
Ray Doetjes <rdoetjes@phonax.com>
2021-06-29 21:37:53 +0100

    more efficient stack

--- asm/arm64_linux/bitcount/bitcount.s
+++ asm/arm64_linux/bitcount/bitcount.s
@@ -3,25 +3,27 @@
 .global _start
 
 _start:
-	mov x0,#256
+	mov x0,#1024
 	bl printNr
 
-	//mov X0, #0			//set the exit code to 0
+	mov X0, #0			//set the exit code to 0
 	bl exit				//call exit
-
 
 //Print value in X0 as an unisgned int to screen
 printNr:
 	stp x29, x30, [sp, #-16]!
+	stp x5, x7, [sp, #-16]!
+	stp x2, x3, [sp, #-16]!
+	stp x0, x1, [sp, #-16]!
 
 	mov x7, #10
 	mov x5, #0
-	mov x6, #0
+	sub sp, sp, #128
 printNr_Count:
 	udiv x2, x0, x7
 	msub x3, x2, x7, x0
-	str x3, [sp, #-16]!
-	add x5,x5, #1
+	add x5, x5, #1
+	strb w3, [sp, x5]
 	mov x0, x2
 	cmp x0, #0
 	bne printNr_Count
@@ -30,15 +32,20 @@ printNr_print:
 	cmp x5, #00
 	beq printNr_exit
 
-	ldr x3, [sp], #+16
+	ldrb w3, [sp, x5]
 	ldr X1,=char
-	add x3, x3, 48
-	str x3, [x1]
-	mov x2, #1
+	add w3, w3, 48
+	strb w3, [x1]
+	mov x2,#1 
 	bl print
 	subs x5, x5, #1
 	bne printNr_print
+
 printNr_exit:
+	add sp, sp, #128
+	ldp x0, x1, [sp], #16
+	ldp x2, x3, [sp], #16
+	ldp x5, x7, [sp], #16
 	ldp x29, x30, [sp], #16
 	ret 
 
@@ -47,15 +54,15 @@ printNr_exit:
 //X2 is string length of the string
 print:
 	stp x29, x30, [sp, #-16]!
-	stp x0, x1, [sp, #-16]!
 	stp x2, x8, [sp, #-16]!
+	stp x0, x1, [sp, #-16]!
 
 	mov X0, #1			//set X0 to point to standard out
 	mov X8, #64			//write syscall 
 	svc #0				//call system call (64 => write)
 
-	ldp x2, x8, [sp], #16
 	ldp x0, x1, [sp], #16
+	ldp x2, x8, [sp], #16
 	ldp x29, x30, [sp], #16
 	ret				//return to caller
 
@@ -68,4 +75,4 @@ exit:
 
 .data
 char:
-	.fill 2, 1, 0
+	.fill 1, 1, 0
