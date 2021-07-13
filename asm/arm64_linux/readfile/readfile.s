bece6184c29f15f9c757782df4d175648cca5568
Ray Doetjes <rdoetjes@phonax.com>
2021-07-13 22:45:32 +0100

    error handling

--- asm/arm64_linux/readfile/readfile.s
+++ asm/arm64_linux/readfile/readfile.s
@@ -12,15 +12,20 @@ _start:
 
 	m_openFile file_name, O_RDONLY
 	mov x11, x0		//keep copy of file desc
+	cmp x0, #0
+	blt open_file_error
 
 more_data:
 	m_readFile x11, file, file_len
 	cmp  x0, #0
-	beq no_more_data
+	beq close_file
 	m_print file, x0
-	bne more_data
-no_more_data:
+	bne close_file
 
+open_file_error:
+	m_print error, error_len
+
+close_file:
 	m_closeFile x0
 
 	mov x0, #0
@@ -31,8 +36,11 @@ no_more_data:
 prompt:		.ascii "enter file name: "
 prompt_len = . - prompt
 
+error:		.ascii "Couldn't open file!\n"
+error_len = . - error
+
 file_name:	.fill 255,1,0
 file_name_len = . - file_name
 
-file:	.fill 255,1,0
+file:		.fill 255,1,0
 file_len = . - file
