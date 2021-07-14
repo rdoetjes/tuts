d612ff75797d826a78d066ad4b0a754a48029fd8
Ray Doetjes <rdoetjes@phonax.com>
2021-07-14 16:11:21 +0100

    sys_exit to make exit unique for main

--- asm/arm64_linux/readfile/system.s
+++ asm/arm64_linux/readfile/system.s
@@ -5,7 +5,7 @@
 
 //Exit to operating system, X0 will contain the exit code
 //X0 contains exit code
-exit:
+sys_exit:
 	mov X8, #93			//exit system call
 	svc #0				//call system call (93 => exit)
 	ret				//this won't be called because exit will terminate program before we get here
