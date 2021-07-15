4938f7737ca9e4026ed35c3a3f2ad0370e02752d
Ray Doetjes <rdoetjes@phonax.com>
2021-07-15 19:52:15 +0100

    knigtrider scanner

--- asm/arm64_linux/gpiofile/gpiofile.s
+++ asm/arm64_linux/gpiofile/gpiofile.s
@@ -17,17 +17,25 @@ _start:
 	m_gpio_setDirectionOut p27
 reset:
 	mov x0, #1
-loop:
+loop_up:
 	bl sys_gpioByte
-loop1:
-	mov x5, x0
+	mov x5, x0			//nano changes x0, so we safe it
 	m_nanosleep
-	mov x0, x5
+	mov x0, x5			//restore x0 after nano call
 
 	lsl x0, x0, #1
-	cmp x0, #256
-	beq reset
-	b loop
+	cmp x0, #128
+	bne loop_up
+loop_down:
+	bl sys_gpioByte
+        mov x5, x0                      //nano changes x0, so we safe it
+        m_nanosleep
+        mov x0, x5                      //restore x0 after nano call
+
+        lsr x0, x0, #1
+        cmp x0, #1
+	bne loop_down
+	b loop_up
 
 exit:
 	m_exit 0
@@ -35,4 +43,4 @@ exit:
 .align 8
 .data
 timespecsec:	.dword 0
-timespecnano:	.dword 600000000
+timespecnano:	.dword 200000000
