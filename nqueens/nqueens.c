d0e1feb7995da84d6034737f093b7e73e32607a1
Raymond Doetjes <rdoetjes@phonax.com>
2023-02-19 15:39:22 +0100

    flow back on the stack not wokring yet

--- nqueens/nqueens.c
+++ nqueens/nqueens.c
@@ -51,136 +51,80 @@ void show_board(int board[N][N]) {
     }
 }
 
-bool is_x_empty(int board[N][N], int y){
-    for(int x=0; x < N; x++){
-        if(board[y][x]>0) return false;
-    }
-    return true;
-}
-
-bool is_y_empty(int board[N][N], int x){
-    for(int y=0; y < N; y++){
-        if(board[y][x]>0) return false;
-    }
-    return true;
-}
-
-bool are_diagonals_clear(int board[N][N], QUEEN queens[N]){
+bool legal_move(QUEEN queens[N]){
     for (int q=0; q<N; q++){
-        
-        int x = queens[q].x;
-        int y = queens[q].y;
+        if (queens[q].t == -1) continue;
 
-        for(int tx=x; tx<N; tx++){
-            for(int ty=y; ty<N; ty++){
-                if(board[y][x] >0 ) return false;
-            }
-        }
+        for(int qc=0; qc<N; qc++){
+            if (qc==q || queens[qc].t<0 || queens[qc].x<0 || queens[qc].y<0) continue;
 
-        for(int tx=x; tx<=0; tx--){
-            for(int ty=y; ty<N; ty--){
-                if(board[y][x] >0 ) return false;
-            }
-        }
+            if(queens[q].x == queens[qc].x) return false;
 
-        for(int tx=x; tx<=0; tx--){
-            for(int ty=y; ty<N; ty++){
-                if(board[y][x] >0 ) return false;
-            }
-        }
-
-        for(int tx=x; tx<N; tx++){
-            for(int ty=y; ty>=0; ty--){
-                if(board[y][x] >0 ) return false;
-            }
+            if(queens[q].y == queens[qc].y) return false;
+            
+            if( abs(queens[q].y - queens[qc].y) == abs(queens[q].x - queens[qc].x) ) return false;
         }
     }
     return true;
-}
-
-int get_next_movable_queen_idx(QUEEN queens[N], int queen_idx){
-    if (queen_idx==N-1) queen_idx = -1;
-
-    for(int i=queen_idx+1; i < N; i++)
-        if (queens[i].t == 1) return i;
-
-    for(int i=0; i < queen_idx; i++)
-        if (queens[i].t == 1) return i;
-
-    return -1;
 }
 
 int get_next_new_queen_idx(QUEEN queens[N]){
     for(int i=0; i < N; i++)
-        if (queens[i].t == 0) return i;
+        if (queens[i].t == -1) return i;
     return 0;
 }
 
-void swap_queen_with_next_one_over(int board[N][N], QUEEN queens[N], int i){
-    if (are_diagonals_clear(board, queens))
-        return;
+int find_empty_col(QUEEN queens[N]){
+    int used[N];
+    for(int i=0; i<N; i++)
+        used[i] = 0;
 
-    int q0 = get_next_movable_queen_idx(queens, i);
-    int q1 = get_next_movable_queen_idx(queens, q0);
+    for(int i=0; i<N; i++)
+        used[queens[i].x ]=1;
 
-    printf("%d %d\n", q0, q1);
-    //clear the old spots of these queens from the board
-    board[queens[q1].y][queens[q1].x] = 0;
-    board[queens[q0].y][queens[q0].x] = 0;
+    for(int i=0; i<N; i++)
+        if ( used[i] == 0) return i;
 
-    printf("q0 %d %d,%d, q1 %d %d,%d\n", q0, queens[q0].y ,queens[q0].x, q1, queens[q1].y, queens[q1].x );
-
-    //safe copy of location for swapping
-    int y = queens[q0].y;
-    //set queen on position q to queen on position i
-    queens[q0].y = queens[q1].y;
-    //set queen on position i to queen on position q
-    queens[q1].y = y; 
-
-    //update the board if the new queen positions
-    board[queens[q0].y][queens[q0].x] = 1;
-    board[queens[q1].y][queens[q1].x] = 1;
-    
-    show_board(board);
-    i = get_next_movable_queen_idx(queens,q1);
-    swap_queen_with_next_one_over(board, queens, i);
+    return -1;
 }
 
-void solve(int board[N][N], QUEEN queens[N], int start_idx){
-    swap_queen_with_next_one_over(board, queens, start_idx);
-}
-
-/*
-Set up the other pieces so that they are never on the same row or column
-as any of the other already placed pieces
-*/
-void set_other_queens_on_board(int board[N][N], QUEEN queens[N], int queen_idx, int x, int y){
-    if (x>=N || y>=N || queen_idx>=N)
+void put_piece(QUEEN queens[N], int queen_idx, int x, int y){
+    if (queen_idx>=N)
         return;
     
-    if (!is_y_empty(board, x)) 
-        set_other_queens_on_board(board, queens, queen_idx, x + 1 ,y);
-    else if (!is_x_empty(board, y)) 
-        set_other_queens_on_board(board, queens, queen_idx, x ,y + 1);
-    else {
-        board[y][x] = 1;
-        queens[queen_idx].x = x; queens[queen_idx].y = y;  queens[queen_idx].t = 1;
-        set_other_queens_on_board(board, queens, queen_idx + 1, x, y);
+     if (y>=N){
+        queens[queen_idx].x = -1;
+        queens[queen_idx].y = -1;
+        return;
+    }
+
+    queens[queen_idx].x = x;
+    queens[queen_idx].y = y;
+    queens[queen_idx].t = 1;
+
+    if (legal_move(queens)){
+        queen_idx++;
+        put_piece(queens, queen_idx, find_empty_col(queens), 0);
+    }
+    else{
+        put_piece(queens, queen_idx, x, y+1);
     }
 }
 
-void set_start_queens_on_board(int board[N][N], QUEEN queens[N], int start_idx){
+void solve(QUEEN queens[N], int start_idx){
+    put_piece(queens, start_idx, find_empty_col(queens), 0);
+}
+
+void reflect_pieces_to_board(int board[N][N], QUEEN queens[N]){
     for(int i=0; i < N; i++) 
         board[queens[i].y][queens[i].x] = queens[i].t;
-
-    set_other_queens_on_board(board, queens, start_idx, 0 ,0);
 }
 
 void init_queens(QUEEN queens[N]){
     for(int x=0; x < N; x++){
         queens[x].x = -1;
         queens[x].y = -1;
-        queens[x].t = 0;
+        queens[x].t = -1;
     }
 }
 
@@ -192,12 +136,12 @@ int main(){
     clear_board(board);
 
     //set the puzzle pieces (2 means puzzle piece that may not be moved)
-    queens[0].x = 0; queens[0].y = 0; queens[0].t = 2;
-    
-    int idx_first_movable_queen = get_next_new_queen_idx(queens);
-    set_start_queens_on_board(board, queens, idx_first_movable_queen);
+    queens[0].x = 0; queens[0].y = 3; queens[0].t = 2;
+    queens[1].x = 2; queens[1].y = 7; queens[1].t = 2;
+
+    solve(queens, get_next_new_queen_idx(queens));
+
+    reflect_pieces_to_board(board, queens);
 
     show_board(board);
-
-    solve(board, queens, idx_first_movable_queen);
 }
