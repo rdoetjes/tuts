48fb18ac56141a40d2d7eceb89b940f6b547a54a
Raymond Doetjes <rdoetjes@phonax.com>
2023-02-19 09:50:02 +0100

    place the none fixed pieces

--- nqueens/nqueens.c
+++ nqueens/nqueens.c
@@ -36,7 +36,7 @@ void show_board(int board[N][N]) {
                     break;
 
                 case 1:
-                    printf("\033[32m Q \033[0m");
+                    printf("\033[33m Q \033[0m");
                     break;
 
                 case 2:
@@ -51,14 +51,50 @@ void show_board(int board[N][N]) {
     }
 }
 
+bool is_x_empty(int board[N][N], int y){
+    for(int x=0; x < N; x++){
+        if(board[y][x]>0) return false;
+    }
+    return true;
+}
+
+bool is_y_empty(int board[N][N], int x){
+    for(int y=0; y < N; y++){
+        if(board[y][x]>0) return false;
+    }
+    return true;
+}
 
 void solve(int board[N][N], QUEEN queens[N], int queen_idx){
     printf("placing queen #%d\n", queen_idx+1);
 }
 
+void set_other_queens_on_board(int board[N][N], QUEEN queens[N], int queen_idx, int x, int y){
+    if (x>=N || y>=N || queen_idx>=N)
+        return;
+    
+    if (!is_y_empty(board, x)) 
+        set_other_queens_on_board(board, queens, queen_idx, x + 1 ,y);
+    else if (!is_x_empty(board, y)) 
+        set_other_queens_on_board(board, queens, queen_idx, x ,y + 1);
+    else {
+        board[y][x] = 1;
+        queens[queen_idx].x = x; queens[queen_idx].y = y;  queens[queen_idx].t = 1;
+        set_other_queens_on_board(board, queens, queen_idx + 1, x, y);
+    }
+}
+
+int get_next_queen_idx(QUEEN queens[N]){
+    for(int i=0; i < N; i++)
+        if (queens[i].t == 0) return i;
+    return 0;
+}
+
 void set_start_queens_on_board(int board[N][N], QUEEN queens[N]){
     for(int i=0; i < N; i++) 
         board[queens[i].y][queens[i].x] = queens[i].t;
+
+    set_other_queens_on_board(board, queens, get_next_queen_idx(queens), 0 ,0);
 }
 
 void init_queens(QUEEN queens[N]){
@@ -67,12 +103,6 @@ void init_queens(QUEEN queens[N]){
         queens[x].y = -1;
         queens[x].t = 0;
     }
-}
-
-int get_next_queen_idx(QUEEN queens[N]){
-    for(int i=0; i < N; i++)
-        if (queens[i].t == 0) return i;
-    return 0;
 }
 
 int main(){
@@ -84,6 +114,8 @@ int main(){
 
     //set the puzzle pieces (2 means puzzle piece that may not be moved)
     queens[0].x = 4; queens[0].y = 4; queens[0].t = 2;
+    queens[1].x = 7; queens[1].y = 0; queens[1].t = 2;
+
 
     set_start_queens_on_board(board, queens);
 
