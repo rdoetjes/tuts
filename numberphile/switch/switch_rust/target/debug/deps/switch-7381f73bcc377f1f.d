c64224a5ac17354305a500b7f758bf0fd14128fa
Raymond Doetjes <rdoetjes@phonax.com>
2023-02-20 10:57:11 +0100

    r

---
+++ numberphile/switch/switch_rust/target/debug/deps/switch-7381f73bcc377f1f.d
@@ -0,0 +1,5 @@
+/Users/rdoetjes/develop/tuts/numberphile/switch/switch_rust/target/debug/deps/switch-7381f73bcc377f1f: src/main.rs
+
+/Users/rdoetjes/develop/tuts/numberphile/switch/switch_rust/target/debug/deps/switch-7381f73bcc377f1f.d: src/main.rs
+
+src/main.rs:
---
+++ numberphile/switch/switch_rust/target/debug/switch.d
@@ -0,0 +1,1 @@
+/Users/rdoetjes/develop/tuts/numberphile/switch/switch_rust/target/debug/switch: /Users/rdoetjes/develop/tuts/numberphile/switch/switch_rust/src/main.rs
