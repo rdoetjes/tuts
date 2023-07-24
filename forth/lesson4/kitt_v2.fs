dbb509acefb71277c52aab82a6dbf933b20d816d
Raymond Doetjes <rdoetjes@phonax.com>
2023-07-24 12:26:44 +0200

    made the code on a single line, to match other lesson

--- forth/lesson4/kitt_v2.fs
+++ forth/lesson4/kitt_v2.fs
@@ -16,31 +16,16 @@ variable high_value
 \ take the number on top of the stack multiply it by 2, display the value in hex, wait 20 ms
 \ repeat this as long as the value on the is less than high_value
 : SCAN_UP
-  BEGIN 
-    DUP high_value @ < WHILE 
-      MUL2 
-      P_HEX 
-      20 ms 
-  REPEAT ;
+  BEGIN DUP high_value @ < WHILE MUL2 P_HEX 20 ms REPEAT ;
 
 \ take the number on top of the stack divide it by 2 it, display the value in hex, wait 20 ms
 \ repeat this as long as the value on the is greater than 1
 : SCAN_DOWN
-  BEGIN 
-    DUP 1 > WHILE 
-      DIV2 
-      P_HEX 
-      20 ms 
-  REPEAT ;
+  BEGIN DUP 1 > WHILE DIV2 P_HEX 20 ms REPEAT ;
 
 \ indefinitely, scan the bit up and down just like the knightrider scanner
 : KITT
-  1 
-  BEGIN 
-    1 1 = WHILE 
-      SCAN_UP 
-      SCAN_DOWN 
-  REPEAT ;
+  1 BEGIN 1 1 = WHILE SCAN_UP SCAN_DOWN REPEAT ;
 
 \ turn key to start the KITT word automatically after the cold start
 KITT `cold
