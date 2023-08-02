3cb169b1891e6343515f47b824e0ea1b8f491fcc
Raymond Doetjes <rdoetjes@phonax.com>
2023-08-03 01:12:11 +0200

    made exp generic

--- forth/resistor/resistor.fs
+++ forth/resistor/resistor.fs
@@ -9,29 +9,29 @@
 8 constant grey  
 9 constant white  
 
-: 10power ( n -- n ) 
-  dup                     \ deuplicate since the = if removes the it from the stack
-  0 = if 1 nip exit then  \ zero case
-  10                      \ multiplier 
-  swap                    \ the number of times we multiply by needs to come 1st
-  begin  
-  dup                     \ dup the number of times the comparison removes it
-  1 > while               \ as long as larger than 1 run the multiply
-    swap 10 *             \ get the current result on top and multiply by 10     
-    swap 1 -              \ get the counter on top and subtract 1     
-  repeat   
-  drop ;                  \ drop the number of multiplies (1) from stack
-    
+: exp ( u1 u2 -- u3 ) \ u3 = u1^u2
+   dup
+   0 = if 1 exit then
+   over swap 1 ?do over * loop nip ;
+
 : r ( n1 n2 n3 -- n4 )
-  10power                 \ only run the 10 power when arg is not 0
-  rot 10 *                \ multiply that second argument (2nd ring) by 10
-  rot +                   \ add the the rings together
-  swap                    \ get the 3rd ring (multiples of 10) on top
-  dup                     \ copy it because 0 > removes it
-  0 > if                  \ if larger than 0 then we multiply the rd ring with the 2rings
+  \ calculate the 3rd ring exponent
+  rot rot >r >r       \ store the first 2 args on return stack
+  10                  \ exponent of 10
+  swap
+  exp                 \ calcualte the 10 ^ 3rd_ring
+  r> r> swap          \ pop the first 2 arguments form return stack
+
+  \ calculate the 1st_ring * 10 + 2nd_ring
+  10 * +               
+  swap                
+
+  \ if 3rd ring calcualted exponent is not 0 than multiply that with the 1_st*10+2_nd 
+  dup                 
+  0 > if                  
     *
   else
-    drop                  \ since we do not multiply we need to drop the 0 here
+    nip
   then 
   ;
 
