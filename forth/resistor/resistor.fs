09b287f56a4ff23e25d15427b2ffd56735a9e9e1
Raymond Doetjes <rdoetjes@phonax.com>
2023-08-02 16:47:45 +0200

    comments align

--- forth/resistor/resistor.fs
+++ forth/resistor/resistor.fs
@@ -10,32 +10,33 @@
 9 constant white  
 
 : 10power ( n -- n ) 
-  10      \ multiplier 
-  swap    \ the number of times we multiply by needs to come 1st
+  10                      \ multiplier 
+  swap                    \ the number of times we multiply by needs to come 1st
   begin  
-  dup     \ dup the number of times the comparison removes it
-  1 > while  \ as long as larger than 1 run the multiply
-    swap     \ get the result (or 10 in first iter) on top of the stack
-    10 *     \ multiply result by 10
-    swap     \ get the the number of multiplies on the stack
-    1 -      \ subtract one from the number of multiplies
+  dup                     \ dup the number of times the comparison removes it
+  1 > while               \ as long as larger than 1 run the multiply
+    swap                  \ get the result (or 10 in first iter) on top of the stack
+    10 *                  \ multiply result by 10
+    swap                  \ get the the number of multiplies on the stack
+    1 -                   \ subtract one from the number of multiplies
   repeat   
-  drop ;     \ drop the number of multiplies (1) from stack
+  drop ;                  \ drop the number of multiplies (1) from stack
     
 : r ( n1 n2 n3 -- n4 )
-  dup \ duplicate the argument the > removed it
-  0 > if 10power then \ only run the 10 power when arg is not 0
-  rot   \ get the second argument on top
-  10 *  \ multiply that second argument (2nd ring) by 10
-  rot   \ get the 1st argument (1st ring) on top 
-  +     \ add the 1st two rings together 
-  swap  \ get the 3rd ring (multiples of 10) on top
-  dup   \ copy it because 0 > removes it
-  0 > if \ if larger than 0 then we multiply the rd ring with the 2rings
+  dup                     \ duplicate the argument the > removed it
+  0 > if 10power then     \ only run the 10 power when arg is not 0
+  rot                     \ get the second argument on top
+  10 *                    \ multiply that second argument (2nd ring) by 10
+  rot                     \ get the 1st argument (1st ring) on top 
+  +                       \ add the 1st two rings together 
+  swap                    \ get the 3rd ring (multiples of 10) on top
+  dup                     \ copy it because 0 > removes it
+  .s
+  0 > if                  \ if larger than 0 then we multiply the rd ring with the 2rings
     *
-    swap drop \ clean up the stuff we don't need
+    swap drop             \ clean up the stuff we don't need
   else 
-     drop   \ if multiplier is 0 then just drop it
+     drop                 \ if multiplier is 0 then just drop it
   then ;
 
 
