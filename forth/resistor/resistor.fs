fe9ac9525d96752f306c6f8bdc1218ffa86d24a4
Raymond Doetjes <rdoetjes@phonax.com>
2023-08-07 14:14:17 +0200

    remove fp from constant

--- forth/resistor/resistor.fs
+++ forth/resistor/resistor.fs
@@ -24,7 +24,7 @@
   10                  \ exponent of 10
   swap                \ swap so we have 10 3rd_ring_value on stack
   exp                 \ catch the 0 case that should return 1
-  r> r> swap          \ pop the first 2 arguments form return stack
+  r> r> swap          \ get the first 2 arguments back from return stack
 
   \ calculate the 1st_ring * 10 + 2nd_ring
   10 * +               
@@ -34,18 +34,18 @@
   dup                 
   0 > if * else nip then ;                 
 
-  : print_resistor_value ( u1 -- ) \ takes the ohm value and turns it into ohm, KOhm or MOhm depending on the size
-    dup
-    1000000 / 1 >= if     \ is the value 1 or higher after dividing by 1e6 than it's mega ohms
-      convert_fp          \ since we want a x.xx value we need to conver number into fixed point
-      1000000,0 f/ f. ." Mohm" cr exit then \ print out the number in fixed point format
+: print_resistor_value ( u1 -- ) \ takes the ohm value and turns it into ohm, KOhm or MOhm depending on the size
+  dup
+  1000000 / 1 >= if     \ is the value 1 or higher after dividing by 1e6 than it's mega ohms
+  convert_fp            \ since we want a x.xx value we need to conver number into fixed point
+  1000000,0 f/ f. ." Mohm" cr exit then \ print out the number in fixed point format
 
-    dup
-    1000 / 1 >= if        \ is the value 1 or higher after dividing by 1e3 than it's Kohm
-      convert_fp          \ convert from number into fixed point
-      1000,0 f/ f. ." Kohm" cr exit then
+  dup
+  1000 / 1 >= if        \ is the value 1 or higher after dividing by 1e3 than it's Kohm
+  convert_fp            \ convert from number into fixed point
+  1000,0 f/ f. ." Kohm" cr exit then
 
-    . ." Ohm" cr ;        \ number as neither divisabke by 1e6 and 1e3 so we treat it as ohms
+  . ." Ohm" cr ;        \ number as neither divisabke by 1e6 and 1e3 so we treat it as ohms
 
 : resistor ( u1, u2, u3 -- ) \ calculate the value of a resistor by passing in the colours as u1 u2 and u3
-  ohm print_resistor_value ;
+  ohm print_resistor_value ;  \ convert to ohms and then print into human readdable format
