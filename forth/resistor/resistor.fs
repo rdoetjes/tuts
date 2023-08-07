f621b568085538c495ddbaeb6f6e61eb6ece1fdf
Raymond Doetjes <rdoetjes@phonax.com>
2023-08-07 14:03:38 +0200

    comments

--- forth/resistor/resistor.fs
+++ forth/resistor/resistor.fs
@@ -12,10 +12,10 @@
 
 : exp ( u1 u2 -- u3 ) \ u3 = u1^u2
    dup
-   0 = if rot 2drop 1 exit then
+   0 = if rot 2drop 1 exit then \ drop the 10 and 0 that are passed
    over swap 1 ?do over * loop nip ; 
 
-: convert_fp
+: convert_fp ( u1 -- u1 u2 ) \ turn into a 'fp'
   1,0 * swap ;
 
 : ohm ( u1 u2 u3 -- u4 )
@@ -34,16 +34,16 @@
   dup                 
   0 > if * else nip then ;                 
 
-  : into_hr_fmt
+  : into_hr_fmt ( u1 -- ) \ takes the ohm value and turns it into ohm, KOhm or MOhm depending on the size
     dup
-    1000000 / 1 >= if 
-      convert_fp 
-      1000000,0 f/ f. ." MOhm" cr exit then
+    1000000 / 1 >= if     \ is the value 1 or higher after dividing by 1e6 than it's mega ohms
+      convert_fp          \ since we want a x.xx value we need to conver number into fixed point
+      1000000,0 f/ f. ." M ohm" cr exit then \ print out the number in fixed point format
 
     dup
-    1000 / 1 >= if 
-      convert_fp 
-      1000,0 f/ f. ." KOhm" cr exit then
+    1000 / 1 >= if        \ is the value 1 or higher after dividing by 1e3 than it's Kohm
+      convert_fp          \ convert from number into fixed point
+      1000,0 f/ f. ." K ohm" cr exit then
 
-    . ." Ohm" cr ;
+    . ." Ohm" cr ;        \ number as neither divisabke by 1e6 and 1e3 so we treat it as ohms
 
