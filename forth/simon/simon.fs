d0d483866eb885fbe0a248ef6e98d834f1567d69
Raymond Doetjes <rdoetjes@phonax.com>
2023-08-21 21:41:15 +0200

    comments

--- forth/simon/simon.fs
+++ forth/simon/simon.fs
@@ -1,15 +1,15 @@
 rng import
 pin import
 
-30 constant max-moves
+30 constant max-moves \ the maximum amount of steps in sequence
 
-variable moves max-moves allot \ maximum moves in simon
+variable moves max-moves allot \ array that holds the sequence
 
-variable step 
+variable step   \ current step of the sequence
 1 step !
 
-variable speed
-250 speed !
+variable speed  \ speed of simon showing the sequence (gets faster every 10 steps)
+250 speed !     \ default value 
 
 : add-move ( value n --) \ adds the value to moves array[n]
   moves + c! ;
@@ -23,7 +23,7 @@ variable speed
 : gen-move-seq ( -- ) \ gen. the 31 different random values for the game
   max-moves 0 do gen-move i add-move loop ;
 
-: setup ( -- ) \ sets up the pins
+: setup ( -- ) \ sets up the pins in such away we can uses maths to calculate step value, led pin and switch from one or the other
   2 output-pin 0 2 pin!
   3 output-pin 0 3 pin!
   4 output-pin 0 4 pin!
@@ -34,10 +34,10 @@ variable speed
   9 input-pin 9 pull-up-pin ;
 
 : reset-game ( -- ) \ reset the game, set step to 0 and gen. new sequence
-  1 step !
-  gen-move-seq ;
+  1 step !        \ set step to first step of the sequence
+  gen-move-seq ;  \ generate the 30 random steps whichs make up the sequence
 
-: toggle-pin-ms ( ms move -- ) \ sets the pin corresponded by move (+2 to get gpio) on and of ms millisec
+: toggle-pin-ms ( ms move -- ) \ sets the pin corresponding to move (+2 to get gpio) on and of for ms millisec
   swap
   dup
   2 + toggle-pin
@@ -55,7 +55,7 @@ variable speed
   loop 
 ;
 
-: key-down ( n -- n ) \ lights up the led a maximum of ms to corresponding led and returns the move 0,1,2 or 3
+: key-down ( n -- n ) \ lights up the led as long as pressed and returns the step value calculated from switch number (- 6)
   depth 2 = if swap drop then \ sometimes we seem to have a superious entry keybouce?
   dup 4 - toggle-pin
   begin 100 ms dup -1 swap pin@ = until
@@ -63,7 +63,7 @@ variable speed
   6 -
   ;
 
-: poll-keys ( -- ) \ checks for a keypress and timesout after 2000ms
+: poll-keys ( -- ) \ checks for a keypress and timeout after ~1500ms
   0
   begin 
   10 ms
@@ -74,14 +74,15 @@ variable speed
     0 9 pin@ = if drop 9 key-down exit then 
   again ; 
 
-: cs depth 0 do . loop ; 
+: cs depth 0 > if depth 0 do drop loop then ; \ clear the stacl
 
 : game-over
+  \ turn all leds on to indicate game over
   1 2 pin!
   1 3 pin!
   1 4 pin!
   1 5 pin!
-  depth 0 > if cs then \ clear stack 
+  cs \ clear stack 
 ;
 
 : players-move ( step -- n) \ reads the buttons and checks the value
@@ -97,19 +98,21 @@ variable speed
   reset-game
   1000 ms
   begin
-    step @  9 <= if 300 speed ! then
+    \ speed up every 10 steps 
+    step @  9 <= if 300 speed ! then 
     step @ 10 >= if 200 speed ! then
     step @ 20 >= if 150 speed ! then
 
     \ simon's move
     step @ simons-move
 
-   \ do user
+    \ do user
     step @ players-move
     -1 = if game-over exit then
 
+    \ no game over do next step in sequence
     step @ 1 + step !
     
-    800 ms
-    step @ max-moves =
+    800 ms  \ wait 800 ms and then let simon start next sequence
+    step @ max-moves = \ did we reach the whole sequence no? continue TODO: victory light show after until
   until ;
