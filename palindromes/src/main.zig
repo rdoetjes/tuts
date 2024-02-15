22c9c7efeadb106026c7c151b7f312606d1113f1
Raymond Doetjes <rdoetjes@phonax.com>
2024-02-15 17:50:23 +0100

    comments

--- palindromes/src/main.zig
+++ palindromes/src/main.zig
@@ -2,6 +2,11 @@ const std = @import("std");
 const stdin = std.io.getStdIn();
 const stdout = std.io.getStdOut().writer();
 
+// takes the first charachter of s and compaires it to the last character of s
+// when they are the same, then the 2nd charachter of s is compared to the 2nd to last character of s
+// when they are the same, then the 3rd charachter of s is compared to the 3rd to last character of s etc etc
+// This way half of the string is checked and when that half matches the other half, then the string is a palindrome
+// when a character doesn't match, then the string is not a palindrome and we return false bailing out early.
 fn is_palindrome(s: []const u8) bool {
     for (0..s.len / 2) |i| {
         const ts = std.ascii.toLower(s[i]);
@@ -22,6 +27,9 @@ test "palindrome test" {
     try std.testing.expectEqual(false, is_palindrome("head"));
 }
 
+// we read a list of words delimited by newlines from stdin
+// each word is checked to see if it is a palindrome
+// when it is, it's printed to the stdout
 pub fn main() !void {
     const std_reader = stdin.reader();
     var br = std.io.bufferedReader(std_reader);
