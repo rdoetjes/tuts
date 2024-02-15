d827b9ba384c0bef8b5e22bd0e333f7fe4dca3db
Raymond Doetjes <rdoetjes@phonax.com>
2024-02-15 12:42:20 +0100

    print

--- palindromes/src/main.zig
+++ palindromes/src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const in = std.io.getStdIn();
+const stdout = std.io.getStdOut().writer();
 
 fn is_palindrome(s: []const u8) bool {
     for (0..s.len / 2) |i| {
@@ -10,18 +11,18 @@ fn is_palindrome(s: []const u8) bool {
     return true;
 }
 
+test "palindrome test" {
+    try std.testing.expectEqual(true, is_palindrome("abba"));
+    try std.testing.expectEqual(false, is_palindrome("head"));
+}
+
 pub fn main() !void {
     const std_reader = std.io.getStdIn().reader();
     var br = std.io.bufferedReader(std_reader);
     var buffer: [4096]u8 = undefined;
     while (try br.reader().readUntilDelimiterOrEof(&buffer, '\n')) |l| {
         if (is_palindrome(l)) {
-            try std.io.getStdOut().writer().print("{s}\n", .{l});
+            try stdout.print("{s}\n", .{l});
         }
     }
-}
-
-test "palindrome test" {
-    try std.testing.expectEqual(true, is_palindrome("abba"));
-    try std.testing.expectEqual(false, is_palindrome("head"));
 }
