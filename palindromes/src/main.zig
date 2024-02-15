ec99b89fb75ce8ccac78a1d48fe20b7e1d81df59
Raymond Doetjes <rdoetjes@phonax.com>
2024-02-15 13:06:07 +0100

    to_lower added

--- palindromes/src/main.zig
+++ palindromes/src/main.zig
@@ -2,9 +2,24 @@ const std = @import("std");
 const in = std.io.getStdIn();
 const stdout = std.io.getStdOut().writer();
 
+fn char_to_lower(c: u8) u8 {
+    if (c >= 65 and c <= 90) {
+        return c | 32;
+    }
+    return c;
+}
+
+test "to_lower test" {
+    try std.testing.expectEqual('u', char_to_lower('U'));
+    try std.testing.expectEqual('a', char_to_lower('a'));
+}
+
 fn is_palindrome(s: []const u8) bool {
     for (0..s.len / 2) |i| {
-        if (s[i] != s[s.len - i - 1]) {
+        const ts = char_to_lower(s[i]);
+        const te = char_to_lower(s[s.len - i - 1]);
+
+        if (ts != te) {
             return false;
         }
     }
