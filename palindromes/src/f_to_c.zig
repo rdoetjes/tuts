a60e68d670b94973858a6c979781d2214d1f86d9
Raymond Doetjes <rdoetjes@phonax.com>
2024-02-20 19:04:16 +0100

    test

--- palindromes/src/f_to_c.zig
+++ palindromes/src/f_to_c.zig
@@ -9,8 +9,38 @@ fn convert_to_celsius(fahrenheit: f64) f64 {
 
 test "to celsius" {
     try std.testing.expectEqual(0.0, convert_to_celsius(32));
-    try std.testing.expectEqual(-8.888888888888897e-01, convert_to_celsius(30.4));
-    try std.testing.expectEqual(2.627777777777778e+01, convert_to_celsius(79.3));
+    try std.testing.expectApproxEqAbs(-0.888888, convert_to_celsius(30.4), 0.000001);
+    try std.testing.expectEqual(-40.0, convert_to_celsius(-40));
+    try std.testing.expectApproxEqAbs(100, convert_to_celsius(212), 0.000001);
+    try std.testing.expectApproxEqAbs(-273.15, convert_to_celsius(-459.67), 0.000001);
+}
+
+fn print_convert_record(l: []u8) void {
+    var it = std.mem.splitAny(u8, l, " C");
+
+    // get the temperature part of the <temp> <unit> pair
+    if (it.next()) |temperature| {
+        // check if the unit part is F for fahernheit
+        if (std.mem.eql(u8, it.peek() orelse "", "F")) {
+            // convert the fharenheit temperature to celsius and print the new value in C
+            const fahrenheit = std.fmt.parseFloat(f64, temperature) catch |err| {
+                std.debug.print("Error: {s} value: {s} cannot convert\n", .{ @errorName(err), temperature });
+                return;
+            };
+
+            //if the unit is not F then print the original value
+            stdout.print("{d:.1} C\n", .{convert_to_celsius(fahrenheit)}) catch |err| {
+                std.debug.print("Error: {s}\n", .{@errorName(err)});
+                return;
+            };
+        } else {
+            // print the original line as there's no need to convert
+            stdout.print("{s}\n", .{l}) catch |err| {
+                std.debug.print("Error: {s}\n", .{@errorName(err)});
+                return;
+            };
+        }
+    }
 }
 
 // we read a list of words delimited by newlines from stdin
@@ -25,22 +55,6 @@ pub fn main() !void {
         std.debug.print("Error: {s}", .{@errorName(err)});
         return;
     }) |l| {
-        var it = std.mem.splitAny(u8, l, " C");
-
-        // get the temperature part of the <temp> <unit> pair
-        if (it.next()) |temperature| {
-            // check if the unit part is F for fahernheit
-            if (std.mem.eql(u8, it.peek() orelse "", "F")) {
-                // convert the fharenheit temperature to celsius and print the new value in C
-                const fahrenheit = std.fmt.parseFloat(f64, temperature) catch |err| {
-                    std.debug.print("Error: {s} value: {s} cannot convert\n", .{ @errorName(err), temperature });
-                    continue;
-                };
-                //if the unit is not F then print the original value
-                try stdout.print("{d:.1} C\n", .{convert_to_celsius(fahrenheit)});
-            } else {
-                std.debug.print("{s}\n", .{l});
-            }
-        }
+        print_convert_record(l);
     }
 }
