9f4f8e981850479a7f18125c56d837a256324f32
Raymond Doetjes <rdoetjes@phonax.com>
2024-02-21 07:59:49 +0100

    return str

--- palindromes/src/f_to_c.zig
+++ palindromes/src/f_to_c.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const stdin = std.io.getStdIn();
 const stdout = std.io.getStdOut().writer();
-const BUFFER_SIZE = 1024;
+const BUFFER_SIZE = 50;
 
 fn convert_to_celsius(fahrenheit: f64) f64 {
     return (fahrenheit - 32) * 5.0 / 9.0;
@@ -15,7 +15,7 @@ test "to celsius" {
     try std.testing.expectApproxEqAbs(-273.15, convert_to_celsius(-459.67), 0.000001);
 }
 
-fn print_convert_record(l: []u8) void {
+fn convert_record(l: []const u8, converted_line: []u8) !void {
     var it = std.mem.splitAny(u8, l, " C");
 
     // get the temperature part of the <temp> <unit> pair
@@ -27,20 +27,26 @@ fn print_convert_record(l: []u8) void {
                 std.debug.print("Error: {s} value: {s} cannot convert\n", .{ @errorName(err), temperature });
                 return;
             };
-
             //if the unit is not F then print the original value
-            stdout.print("{d:.1} C\n", .{convert_to_celsius(fahrenheit)}) catch |err| {
-                std.debug.print("Error: {s}\n", .{@errorName(err)});
-                return;
-            };
+            _ = try std.fmt.bufPrint(converted_line, "{d:.1} C\n", .{convert_to_celsius(fahrenheit)});
         } else {
-            // print the original line as there's no need to convert
-            stdout.print("{s}\n", .{l}) catch |err| {
-                std.debug.print("Error: {s}\n", .{@errorName(err)});
-                return;
-            };
+            _ = try std.fmt.bufPrint(converted_line, "{s}\n", .{l});
         }
     }
+}
+
+test "convert record C" {
+    var converted_line = std.mem.zeroes([BUFFER_SIZE]u8);
+    try convert_record("32 C", &converted_line);
+    try std.testing.expectStringStartsWith(&converted_line, "32 C\n");
+
+    converted_line = std.mem.zeroes([BUFFER_SIZE]u8);
+    try convert_record("30 F", &converted_line);
+    try std.testing.expectStringStartsWith(&converted_line, "-1.1 C\n");
+
+    converted_line = std.mem.zeroes([BUFFER_SIZE]u8);
+    try convert_record("72.2 F", &converted_line);
+    try std.testing.expectStringStartsWith(&converted_line, "22.3 C\n");
 }
 
 // we read a list of words delimited by newlines from stdin
@@ -55,6 +61,16 @@ pub fn main() !void {
         std.debug.print("Error: {s}", .{@errorName(err)});
         return;
     }) |l| {
-        print_convert_record(l);
+        var converted_line = std.mem.zeroes([BUFFER_SIZE]u8);
+
+        convert_record(l, &converted_line) catch |err| {
+            std.debug.print("Error: {s}", .{@errorName(err)});
+            continue;
+        };
+
+        stdout.print("{s}", .{converted_line}) catch |err| {
+            std.debug.print("Error: {s}\n", .{@errorName(err)});
+            continue;
+        };
     }
 }
