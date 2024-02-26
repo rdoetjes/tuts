5b6130adc3d91f511da2f1ec68f06e2f4844c6df
Raymond Doetjes <rdoetjes@phonax.com>
2024-02-26 18:29:15 +0100

    naming

--- f_to_c_go/fahrenheit.go
+++ f_to_c_go/fahrenheit.go
@@ -12,23 +12,25 @@ func convert_c_to_f(c float64) float64 {
 	return (c - 32) * 5.0 / 9.0
 }
 
-func convert_line(line string) (string, error) {
+func conform_line(line string) (string, error) {
 	columns := strings.Split(line, " ")
 	if columns[1] != "F" {
 		return line, nil
 	}
 
-	f, err := strconv.ParseFloat(columns[0], 64)
+	fahrenheit, err := strconv.ParseFloat(columns[0], 64)
 	if err != nil {
 		return "", err
 	}
-	s_f := fmt.Sprintf("%.1f C", convert_c_to_f(f))
-
-	return s_f, nil
+	return fmt.Sprintf("%.1f C", convert_c_to_f(fahrenheit)), nil
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "opening standard input:", err)
+	}
+
 	for {
 		if !scanner.Scan() {
 			if err := scanner.Err(); err != nil {
@@ -37,12 +39,12 @@ func main() {
 			break
 		}
 
-		line, err := convert_line(scanner.Text())
+		celcius_conformed_record, err := conform_line(scanner.Text())
 		if err != nil {
-			println("Error converting line:", err)
+			fmt.Println("Error converting line:", err)
 			continue
 		}
 
-		println(line)
+		println(celcius_conformed_record)
 	}
 }
