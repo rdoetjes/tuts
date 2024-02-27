6d88177ca59d1ece4974f9da9789b78aae1c1055
Raymond Doetjes <rdoetjes@phonax.com>
2024-02-27 06:56:15 +0100

    fps function

--- go-cv/main.go
+++ go-cv/main.go
@@ -9,6 +9,16 @@ import (
 	"gocv.io/x/gocv"
 )
 
+func add_fps(frame *gocv.Mat, avg_tot float64, framecount int64, last_time int64) {
+	current_time := time.Now().UnixNano()
+	avg_tot += 1 / ((float64(current_time) - float64(last_time)) / 1e9)
+	framecount += 1
+
+	fps := fmt.Sprintf("%.0f", avg_tot/float64(framecount))
+	pt := image.Point{X: 100, Y: 100}
+	gocv.PutText(frame, fps, pt, gocv.FontHersheyPlain, 1.2, color.RGBA{255, 0, 255, 1}, 2)
+}
+
 func main() {
 	webcam, _ := gocv.VideoCaptureDevice(1)
 	window := gocv.NewWindow("Wbecam")
@@ -22,15 +32,7 @@ func main() {
 		if window.WaitKey(1) != -1 {
 			break
 		}
-
-		current_time := time.Now().UnixNano()
-		avg_tot += 1 / ((float64(current_time) - float64(last_time)) / 1e9)
-		framecount += 1
-
-		fps := fmt.Sprintf("%.0f", avg_tot/float64(framecount))
-		pt := image.Point{X: 100, Y: 100}
-		gocv.PutText(&img, fps, pt, gocv.FontHersheyPlain, 1.2, color.RGBA{255, 0, 255, 1}, 2)
-
+		add_fps(&img, avg_tot, framecount, last_time)
 		window.IMShow(img)
 	}
 }
