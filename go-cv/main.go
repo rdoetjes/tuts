9800c25874282c1f5148c3d9bc15ca9c03530883
Raymond Doetjes <rdoetjes@phonax.com>
2024-02-27 07:39:38 +0100

    scoping of func and type

--- go-cv/main.go
+++ go-cv/main.go
@@ -9,13 +9,13 @@ import (
 	"gocv.io/x/gocv"
 )
 
-type fps struct {
+type Fps struct {
 	avg_tot    float64
 	framecount int64
 	last_time  int64
 }
 
-func AddFpsOnFrame(frame *gocv.Mat, fps_data *fps) {
+func AddFpsOnFrame(frame *gocv.Mat, fps_data *Fps) {
 	current_time := time.Now().UnixNano()
 	fps_data.avg_tot += 1 / ((float64(current_time) - float64(fps_data.last_time)) / 1e9)
 	fps_data.framecount += 1
@@ -26,7 +26,7 @@ func AddFpsOnFrame(frame *gocv.Mat, fps_data *fps) {
 }
 
 func main() {
-	fps_data := fps{avg_tot: 0, framecount: 0, last_time: 0}
+	fps_data := Fps{avg_tot: 0, framecount: 0, last_time: 0}
 	webcam, err := gocv.VideoCaptureDevice(1)
 	if err != nil {
 		panic(err)
