9e8af1d52f9a7f347658797fa2703fc6510cbfc1
Raymond Doetjes <rdoetjes@phonax.com>
2024-02-27 07:25:38 +0100

    struct

--- go-cv/main.go
+++ go-cv/main.go
@@ -9,33 +9,36 @@ import (
 	"gocv.io/x/gocv"
 )
 
-func add_fps(frame *gocv.Mat, avg_tot float64, framecount int64, last_time int64) {
-	current_time := time.Now().UnixNano()
-	avg_tot += 1 / ((float64(current_time) - float64(last_time)) / 1e9)
-	framecount += 1
+type fps struct {
+	avg_tot    float64
+	framecount int64
+	last_time  int64
+}
 
-	fps := fmt.Sprintf("%.0f", avg_tot/float64(framecount))
+func add_fps(frame *gocv.Mat, fps_data *fps) {
+	current_time := time.Now().UnixNano()
+	fps_data.avg_tot += 1 / ((float64(current_time) - float64(fps_data.last_time)) / 1e9)
+	fps_data.framecount += 1
+	fps := fmt.Sprintf("%.0f", fps_data.avg_tot/float64(fps_data.framecount))
 	pt := image.Point{X: 100, Y: 100}
 	gocv.PutText(frame, fps, pt, gocv.FontHersheyPlain, 1.2, color.RGBA{255, 0, 255, 1}, 2)
+	fps_data.last_time = current_time
 }
 
 func main() {
+	fps_data := fps{avg_tot: 0, framecount: 0, last_time: time.Now().UnixNano()}
 	webcam, err := gocv.VideoCaptureDevice(1)
 	if err != nil {
 		panic(err)
 	}
 	window := gocv.NewWindow("WebCam")
 	img := gocv.NewMat()
-	var framecount int64 = 0
-	var avg_tot float64 = 0
-
 	for {
-		last_time := time.Now().UnixNano()
 		webcam.Read(&img)
 		if window.WaitKey(1) != -1 {
 			break
 		}
-		add_fps(&img, avg_tot, framecount, last_time)
+		add_fps(&img, &fps_data)
 		window.IMShow(img)
 	}
 }
