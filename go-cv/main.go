98c3bbc2ad1631b32340415cefa6ac9c2213dac6
Raymond Doetjes <rdoetjes@phonax.com>
2024-02-27 07:53:04 +0100

    comments

--- go-cv/main.go
+++ go-cv/main.go
@@ -19,12 +19,19 @@ type Fps struct {
 
 // AddFpsOnFrame draws the calculated FPS on the frame
 func AddFpsOnFrame(frame *gocv.Mat, fps_data *Fps) {
+	// store the new data in the struct
 	current_time := time.Now().UnixNano()
 	fps_data.avg_tot += 1 / ((float64(current_time) - float64(fps_data.last_time)) / 1e9)
 	fps_data.framecount += 1
+
+	// calculate fps
 	fps := fmt.Sprintf("%.0f", fps_data.avg_tot/float64(fps_data.framecount))
+
+	// draw the fps on the frame
 	pt := image.Point{X: 10, Y: 20}
 	gocv.PutText(frame, fps, pt, gocv.FontHersheyPlain, 1.2, color.RGBA{255, 0, 255, 1}, 2)
+
+	// update the last_time to the current time, for the next run
 	fps_data.last_time = current_time
 }
 
