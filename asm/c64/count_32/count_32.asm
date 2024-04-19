422eeb516cecae36761e1cb7a5c87efa4718fa77
Raymond Doetjes <rdoetjes@phonax.com>
2024-04-19 15:40:52 +0200

    no clc and adc #0

--- asm/c64/count_32/count_32.asm
+++ asm/c64/count_32/count_32.asm
@@ -6,19 +6,16 @@ loop:
     adc #1
     sta bytes+3
     bcc loop
-    clc
     lda bytes+2
-    adc #1
+    adc #0
     sta bytes+2
     bcc loop
-    clc
     lda bytes+1
-    adc #1
+    adc #0
     sta bytes+1
     bcc loop
-    clc
     lda bytes+0
-    adc #1
+    adc #0
     sta bytes+0
     bcc loop
 
