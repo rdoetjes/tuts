d816d0fba3df61d29c306b8a3faefbef7888d4ca
Raymond Doetjes <rdoetjes@phonax.com>
2024-06-02 12:51:14 +0200

    invert button

--- nuclear_random/main.asm
+++ nuclear_random/main.asm
@@ -81,7 +81,7 @@ wait_for_decay_pulse:
    !await_pulse:
     //poll joystick button, a button press is a tick from the geiger counter
     lda JOYSTICK_2      // read joystick 2
-    and #$10            // check button pressed
+    and #%11101111            // check button pressed
     bne !await_pulse-   // no new decay measured than wait
     rts
 
