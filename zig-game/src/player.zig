d02a9e7968c313ac11bffdf0ec0391cb95b0d381
Raymond Doetjes <rdoetjes@phonax.com>
2024-08-24 19:04:35 +0200

    render order

--- zig-game/src/player.zig
+++ zig-game/src/player.zig
@@ -7,16 +7,19 @@ pub const Player = struct {
      health: u8,
      speed: u8,
      ammo: u8,
+     max_ammo: u8,
      sprite: rl.Texture2D,
 
      pub fn init() !Player {
-        return .{
+         const start_ammo = 100;
+         return .{
             .pos = .{ .x = config.SCREEN_HEIGHT/2, .y = config.SCREEN_WIDTH/2, },
             .speed = 3,
             .health = 10,
-            .ammo = 255,
+            .max_ammo = start_ammo,
+            .ammo = start_ammo,
             .sprite = rl.loadTexture("resources/sprites/player.png"),
-        };
+         };
      }
 
      pub fn deinit(self: Player) void {
