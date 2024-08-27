865f2f467e933d499010baeb8e9edb2da4bbdd0e
Raymond Doetjes <rdoetjes@phonax.com>
2024-08-27 20:24:50 +0200

    added bargraph

--- zig-game/src/bar_cauge.zig
+++ zig-game/src/bar_cauge.zig
@@ -2,6 +2,7 @@ const rl = @import("raylib");
 const config = @import("config.zig");
 const pos = @import("position.zig");
 
+//shows a health vbar meter
 pub const BarCauge = struct {
     pos: pos.Position,
     health: f32,
@@ -19,11 +20,13 @@ pub const BarCauge = struct {
         };
     }
 
+    // print the bargraph on position XY
     pub fn moveToXY(self: *BarCauge, x: i32, y: i32) void {
         self.pos.x = x;
         self.pos.y = y;
     }
 
+    // draw the health amount on the bargraph
     pub fn draw(self: *const BarCauge, health: f32) void {
         rl.drawRectangle(self.pos.x, self.pos.y, @intFromFloat(self.width), @intFromFloat(self.height), rl.Color.red);
         const bar_width = self.width * (health/self.max_health);
