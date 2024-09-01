4966a8ce37b3ba6a0d09553e4c94b124a3446d00
Raymond Doetjes <rdoetjes@phonax.com>
2024-09-01 16:24:59 +0200

    avoiding diagnal moves

--- zig-game/src/game_input.zig
+++ zig-game/src/game_input.zig
@@ -6,27 +6,35 @@ const std = @import("std");
 
 // handle all input and map accordingly to the right method in game_logic.zig
 pub fn handle_input(state: *gs.GameState) !void {
-    if (rl.isKeyDown(rl.KeyboardKey.key_up)) {
-        gl.player_up(state);
-    }
-
-    if (rl.isKeyDown(rl.KeyboardKey.key_down)) {
-        gl.player_down(state);
-    }
-
-    if (rl.isKeyDown(rl.KeyboardKey.key_right)) {
-        gl.player_right(state);
-    }
-
-    if (rl.isKeyDown(rl.KeyboardKey.key_left)) {
-        gl.player_left(state);
-    }
-
     if (rl.isKeyDown(rl.KeyboardKey.key_space)) {
         // avoids the game to fire when in splash screen or gameover screen
         if (state.screen == .playing) {
             try gl.player_fire(state);
         }
+    }
+
+    if (rl.isKeyReleased(rl.KeyboardKey.key_space)) {
+        gl.player_fire_release(state);
+    }
+
+    if (rl.isKeyDown(rl.KeyboardKey.key_up)) {
+        gl.player_up(state);
+        return;
+    }
+
+    if (rl.isKeyDown(rl.KeyboardKey.key_down)) {
+        gl.player_down(state);
+        return;
+    }
+
+    if (rl.isKeyDown(rl.KeyboardKey.key_right)) {
+        gl.player_right(state);
+        return;
+    }
+
+    if (rl.isKeyDown(rl.KeyboardKey.key_left)) {
+        gl.player_left(state);
+        return;
     }
 
     if (rl.isKeyReleased(rl.KeyboardKey.key_s)) {
@@ -40,10 +48,6 @@ pub fn handle_input(state: *gs.GameState) !void {
             state.screen = .splash;
             return;
         }
-    }
-
-    if (rl.isKeyReleased(rl.KeyboardKey.key_space)) {
-        gl.player_fire_release(state);
     }
 
     if (rl.isKeyReleased(rl.KeyboardKey.key_down)) {
