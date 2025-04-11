d95e51e193fc86d2d55fff964a22e06b6b2ffec0
Raymond Doetjes <rdoetjes@phonax.com>
2025-04-11 17:58:13 +0200

    abstarction

--- zig-https-openai/README.md
+++ zig-https-openai/README.md
@@ -1,4 +1,9 @@
 **Zig HTTP Client Example**
+
 We are using the `std.http` module to make an HTTP request to the OpenAI API to generate a joke bout Germans.
 
 We are using Zig 0.14.0 here, a lot as changed since the last version and probably will change again in the future. As it seems the interfaces are not stable yet.
+
+In the main_single_request.zig is a very simple example of how to use the `std.http` module to make an HTTP request to the OpenAI API to generate a joke bout Germans. It doesn't abstract the OpenAI API complexity. This is a good starting point for learning how to use the `std.http` module.
+
+In the main.zig is a more complex example of how to use the `std.http` module to make multiple HTTP requests to the OpenAI API to generate multiple jokes about Germans. It abstracts the OpenAI API complexity. This is a good starting point for learning how to use the `std.http` module.
--- zig-https-openai/src/main.zig
+++ zig-https-openai/src/main.zig
@@ -1,29 +1,11 @@
 const std = @import("std");
+const openai = @import("openai_v1.zig");
 const mem = std.mem;
-const json = std.json;
 
 pub fn main() !void {
     // Initialize allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-
-    // Make client used to make requests using fetch
-    var client = std.http.Client{
-        .allocator = allocator,
-    };
-    defer client.deinit();
-
-    // Request body for the OpenAI API to tell a nice joke about Germans
-    const body = try std.json.stringifyAlloc(allocator, .{
-        .model = "gpt-4o",
-        .messages = .{
-            .{
-                .role = "user",
-                .content = "Tell me a joke about germans In the style of Hans Landa from Ingolorius Bastards no introduction no preludes, the audience knows it is a joke!",
-            },
-        },
-    }, .{});
-    defer allocator.free(body);
 
     // fetch my OpenAI API key from environment variable OPENAI_API_KEY (std.os.getenv no longer works it seems)
     const openai_api_key = std.process.getEnvVarOwned(allocator, "OPENAI_API_KEY") catch {
@@ -32,43 +14,36 @@ pub fn main() !void {
     };
     defer allocator.free(openai_api_key);
 
-    // create the authorization header with the API key as a Bearer token
-    const auth_header = try std.fmt.allocPrint(allocator, "Bearer {s}", .{openai_api_key});
-    defer allocator.free(auth_header);
-
-    const headers = &[_]std.http.Header{
-        .{
-            .name = "Authorization",
-            .value = auth_header,
-        },
-        .{
-            .name = "Content-Type",
-            .value = "application/json",
-        },
+    // Initialize the OpenAI client
+    const v1 = openai.OpenAI_v1.init(allocator, openai_api_key) catch |err| {
+        std.debug.print("Failed to initialize OpenAI client: {any}\n", .{err});
+        std.os.linux.exit(1);
     };
+    defer v1.deinit();
+    v1.set_model("gpt-3-5-turbo");
 
-    // Initialize the response body buffer (ths is of course a u8 arraylist as per standard in zig)
-    var resp_body = std.ArrayList(u8).init(allocator);
-    defer resp_body.deinit();
+    while (true) {
+        var frame_gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        const frame_allocator = frame_gpa.allocator();
+        defer _ = frame_gpa.deinit();
 
-    // perform the fetch request using the header, body and response storage
-    const response = try client.fetch(.{
-        .method = .POST,
-        .location = .{ .url = "https://api.openai.com/v1/chat/completions" },
-        .extra_headers = headers,
-        .payload = body,
-        .response_storage = .{ .dynamic = &resp_body },
-    });
+        // Initialize the response body buffer (ths is of course a u8 arraylist as per standard in zig)
+        var resp_body = std.ArrayList(u8).init(frame_allocator);
+        defer resp_body.deinit();
 
-    //print the content of the JSON response body if http status is ok
-    if (response.status == .ok) {
-        //std.debug.print("The whole Response Body: {s}\n", .{resp_body.items});
-        const parsed = try std.json.parseFromSlice(std.json.Value, allocator, resp_body.items, .{});
-        defer parsed.deinit();
+        // ask openai a question
+        const response = try v1.ask("Tell me a joke about germans In the style of Hans Landa from Ingolorius Bastards no introduction no preludes, the audience knows it is a joke!", &resp_body);
 
-        const message = parsed.value.object.get("choices").?.array.items[0].object.get("message").?.object.get("content").?.string;
-        std.debug.print("\x1b[31mZee joke about zee Germans:\x1b[0m\n \x1b[32m{s}\x1b[0m\n", .{message});
-    } else {
-        std.debug.print("Request failed with status: {any}\n", .{response});
+        //print the content of the JSON response body if http status is ok
+        if (response.status == .ok) {
+            const parsed = try std.json.parseFromSlice(std.json.Value, frame_allocator, resp_body.items, .{});
+            defer parsed.deinit();
+
+            const message = parsed.value.object.get("choices").?.array.items[0].object.get("message").?.object.get("content").?.string;
+            std.debug.print("\x1b[31mZee joke about zee Germans:\x1b[0m\n \x1b[32m{s}\x1b[0m\n", .{message});
+        } else {
+            std.debug.print("Request failed with status: {any}\n", .{response});
+        }
+        std.time.sleep(5 * 1e9);
     }
 }
