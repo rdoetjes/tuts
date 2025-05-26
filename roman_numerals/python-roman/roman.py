f156bdef0d9784067f1b41959f24a983443fcae4
Raymond Doetjes <rdoetjes@phonax.com>
2025-05-26 14:57:56 +0200

    added pyhton as a benchmark too (fuck python, way too slow)

---
+++ roman_numerals/python-roman/roman.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import sys
+
+def to_roman(year: int) -> str:
+    from collections import namedtuple
+
+    Numeral = namedtuple('Numeral', ['roman', 'dec'])
+
+    numerals = [
+        Numeral("M", 1000),
+        Numeral("CM", 900),
+        Numeral("D", 500),
+        Numeral("CD", 400),
+        Numeral("C", 100),
+        Numeral("XC", 90),
+        Numeral("L", 50),
+        Numeral("XL", 40),
+        Numeral("X", 10),
+        Numeral("IX", 9),
+        Numeral("V", 5),
+        Numeral("IV", 4),
+        Numeral("I", 1),
+    ]
+
+    remainder = year
+    result = ""
+    i = 0
+
+    while remainder > 0:
+        if remainder < numerals[i].dec:
+            i += 1
+        else:
+            result += numerals[i].roman
+            remainder -= numerals[i].dec
+
+    return result
+
+def read_stdin(size: int) -> str:
+    data = sys.stdin.read(size)
+    if '\n' in data:
+        data = data[:data.index('\n')]
+    return data.strip()
+
+def main():
+    MAX_LENGTH = 6
+    input_str = read_stdin(MAX_LENGTH-1)
+
+    if len(input_str) >= MAX_LENGTH:
+        sys.stderr.write("Input string too long\n")
+        sys.exit(1)
+
+    try:
+        year = int(input_str)
+    except ValueError:
+        sys.stderr.write("Invalid input\n")
+        sys.exit(1)
+
+    roman = to_roman(year)
+    print(roman)
+
+if __name__ == "__main__":
+    main()
