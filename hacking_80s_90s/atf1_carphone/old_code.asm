d064c637e4fb1edd17c0a485536f533f875a67f0
Raymond Doetjes <rdoetjes@phonax.com>
2025-07-02 14:47:52 +0200

    git commit fixed setup sid

--- hacking_80s_90s/atf1_carphone/old_code.asm
+++ hacking_80s_90s/atf1_carphone/old_code.asm
@@ -3,9 +3,20 @@ BasicUpstart2(main)
         * = $1000         // Program starts here
 
 // --- Timer value (10 ms) ---
-.const timer_value = $268C         // ~9852 decimal
+.const ms10_timer = $268C         // ~9852 decimal
+.const ms600_timer = $FFFF         // and we need 9 of these
 
 main:
+        jsr hz2070
+        jsr setup_sid
+        // jsr setup_timer
+        // jsr setup_timer_600ms
+        // jsr start_timer
+        // jsr setup_timer_10ms
+        // jsr start_timer
+        jmp *
+
+setup_timer:
         sei                 // Disable interrupts
 
         lda #$7f
@@ -18,16 +29,28 @@ main:
         lda #$01
         sta $d01a           // Enable IRQs from CIA #1
 
-        lda #<irq_handler
-        sta $0314           // Set IRQ vector low byte
-        lda #>irq_handler
-        sta $0315           // Set IRQ vector high byte
+        rts
 
-        lda #<timer_value
+setup_timer_10ms:
+        lda #<ms10_timer
         sta $dc04           // CIA1 Timer A low byte
-        lda #>timer_value
+        lda #>ms10_timer
         sta $dc05           // CIA1 Timer A high byte
 
+        lda #<ms10irq
+        sta $0314           // Set IRQ vector low byte
+        lda #>ms10irq
+        sta $0315           // Set IRQ vector high byte
+        rts
+
+setup_timer_600ms:
+        lda #<ms600irq
+        sta $0314           // Set IRQ vector low byte
+        lda #>ms600irq
+        sta $0315           // Set IRQ vector high byte
+        rts
+
+start_timer:
         lda #%00010001      // Start Timer A, continuous mode, count CPU cycles
         sta $dc0e
 
@@ -35,18 +58,87 @@ main:
         sta $dc0d
 
         cli                 // Enable interrupts
+        rts
 
-loop:
-        jmp loop            // Endless loop
+setup_sid:
+        // Set volume
+        lda #$0f       // max volume
+        sta $d418      // store volume
 
-// --- IRQ handler ---
-irq_handler:
+        lda #$00      //attack decay 0
+        sta $d405      
+
+        lda #$f0      //sustain release 0
+        sta $d406
+        
+        // Set control register for pulse wave and gate
+        lda #$11       // triangle
+        sta $d404      // Control register for voice 1
+
+        rts
+
+hz1950:
+        // Set frequency (example: 1950 Hz) PAL
+        lda #$a0       // Low byte of frequency 
+        sta $d400
+        lda #$81       // High byte of frequency 
+        sta $d401
+        rts
+
+hz2070:
+        // Set frequency (example: 2070 Hz) PAL
+        lda #$ea       // Low byte of frequency
+        sta $d400
+        lda #$89       // High byte of frequency 
+        sta $d401
+        rts
+
+// --- IRQ handler 600ms ---
+// we acknowledge the interrupt 9 times, that would lead to a 598 ms delay (close enough)
+ms600irq:
+        irq_counter:
+                .byte 0
+
+        irq_handler:
+                pha
+                txa
+                pha
+                tya
+                pha
+
+                lda $dc0d           // Acknowledge Timer A
+
+                inc irq_counter
+                lda irq_counter
+                cmp #9
+                bne skip_action
+
+                // --- 600 ms elapsed clear counter for next time ---
+                lda #0
+                sta irq_counter
+
+                // Set frequency (example: 0 Hz)
+                lda #$00       // Low byte of frequency 
+                sta $D400
+                lda #$00       // High byte of frequency 
+                sta $D401
+
+        skip_action:
+                pla
+                tay
+                pla
+                tax
+                pla
+                rti
+
+// --- IRQ handler 10ms ---
+ms10irq:
         lda $dc0d           // Acknowledge CIA1 interrupt
 
         // Do something here if needed
         inc $d020           // Flash border color for debug
 
-        jmp $ea31           // Jump to standard IRQ handler (restore registers, etc.)
+        rti
 
 starttelegram:  //S in the string
 .byte %01110010
