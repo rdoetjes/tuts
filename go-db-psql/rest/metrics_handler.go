2a526e792ab5d42e6b075d6d2353d9940f390331
Raymond Doetjes <rdoetjes@phonax.com>
2025-12-10 05:31:09 +0100

    superfluous message fixed

--- go-db-psql/rest/metrics_handler.go
+++ go-db-psql/rest/metrics_handler.go
@@ -8,9 +8,7 @@ import (
 
 // MetricsHandler returns query metrics as JSON
 func MetricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
+	// Let RespondJSON set the Content-Type and status code to avoid
 	api := &RestAPI[interface{}]{}
 	api.RespondJSON(w, http.StatusOK, metrics.GetMetrics())
 }
