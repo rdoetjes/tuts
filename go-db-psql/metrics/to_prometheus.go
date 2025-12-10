a1c1bf72bf7d706bcdc4da0975a5bff5f2ba1cb9
Raymond Doetjes <rdoetjes@phonax.com>
2025-12-10 16:57:43 +0100

    Prometheus works in seconds and we count in ms

--- go-db-psql/metrics/to_prometheus.go
+++ go-db-psql/metrics/to_prometheus.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func durToMilliSec(d time.Duration) float64 {
+func durToSec(d time.Duration) float64 {
 	return float64(d) / 1000
 }
 
@@ -19,10 +19,10 @@ func ConvertMetricsToPrometheus(metrics []byte) (string, error) {
 	var b strings.Builder
 
 	// Global metrics
-	fmt.Fprintf(&b, "api_total_duration_seconds %f\n", durToMilliSec(m.TotalDuration))
+	fmt.Fprintf(&b, "api_total_duration_seconds %f\n", durToSec(m.TotalDuration))
 	fmt.Fprintf(&b, "api_total_errors %d\n", m.TotalErrors)
 	fmt.Fprintf(&b, "api_total_queries %d\n", m.TotalQueries)
-	fmt.Fprintf(&b, "api_average_duration_seconds %f\n", durToMilliSec(m.TotalDuration/time.Duration(m.TotalQueries)))
+	fmt.Fprintf(&b, "api_average_duration_seconds %f\n", durToSec(m.TotalDuration/time.Duration(m.TotalQueries)))
 	fmt.Fprintf(&b, "api_uptime %f\n", time.Since(startTime).Seconds())
 	fmt.Fprintf(&b, "api_qps %d\n", m.Qps)
 	fmt.Fprintf(&b, "api_total_failed_logins %d\n", m.TotalFailedLogins)
@@ -31,10 +31,10 @@ func ConvertMetricsToPrometheus(metrics []byte) (string, error) {
 	for op, om := range m.OperationMetrics {
 		opl := strings.ToLower(op)
 
-		fmt.Fprintf(&b, "api_operation_duration_seconds_avg{operation=\"%s\"} %f\n", opl, durToMilliSec(om.AverageDuration))
-		fmt.Fprintf(&b, "api_operation_duration_seconds_min{operation=\"%s\"} %f\n", opl, durToMilliSec(om.MinDuration))
-		fmt.Fprintf(&b, "api_operation_duration_seconds_max{operation=\"%s\"} %f\n", opl, durToMilliSec(om.MaxDuration))
-		fmt.Fprintf(&b, "api_operation_duration_seconds_total{operation=\"%s\"} %f\n", opl, durToMilliSec(om.TotalDuration))
+		fmt.Fprintf(&b, "api_operation_duration_seconds_avg{operation=\"%s\"} %f\n", opl, durToSec(om.AverageDuration))
+		fmt.Fprintf(&b, "api_operation_duration_seconds_min{operation=\"%s\"} %f\n", opl, durToSec(om.MinDuration))
+		fmt.Fprintf(&b, "api_operation_duration_seconds_max{operation=\"%s\"} %f\n", opl, durToSec(om.MaxDuration))
+		fmt.Fprintf(&b, "api_operation_duration_seconds_total{operation=\"%s\"} %f\n", opl, durToSec(om.TotalDuration))
 		fmt.Fprintf(&b, "api_operation_count{operation=\"%s\"} %d\n", opl, om.Count)
 		fmt.Fprintf(&b, "api_operation_errors{operation=\"%s\"} %d\n", opl, om.NrErrors)
 	}
