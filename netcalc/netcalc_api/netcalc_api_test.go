5ad97d8844a7e6bec759bae10a1ad0c9133b819c
Raymond Doetjes <rdoetjes@phonax.com>
2025-12-20 07:04:05 +0100

    added idr test for rest

--- netcalc/netcalc_api/netcalc_api_test.go
+++ netcalc/netcalc_api/netcalc_api_test.go
@@ -8,7 +8,15 @@ import (
 )
 
 func TestCalculateHandler(t *testing.T) {
-	req, err := http.NewRequest("GET", "/calculate?ip=192.168.1.1&netmask=255.255.255.0", nil)
+	expectedResponse := ApiResponse{
+		IPAddress:        "192.168.1.1",
+		Netmask:          "255.255.252.0",
+		NetworkAddress:   "192.168.0.0",
+		BroadcastAddress: "192.168.3.255",
+		UsableHosts:      1022,
+	}
+
+	req, err := http.NewRequest("GET", "/calculate?ip=192.168.1.1&netmask=255.255.252.0", nil)
 	if err != nil {
 		t.Fatalf("Could not create request: %v", err)
 	}
@@ -19,15 +27,7 @@ func TestCalculateHandler(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	}
-
-	expectedResponse := ApiResponse{
-		IPAddress:        "192.168.1.1",
-		Netmask:          "255.255.255.0",
-		NetworkAddress:   "192.168.1.0",
-		BroadcastAddress: "192.168.1.255",
-		UsableHosts:      254,
+		t.Errorf("1:handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
 	var actualResponse ApiResponse
@@ -36,6 +36,20 @@ func TestCalculateHandler(t *testing.T) {
 	}
 
 	if actualResponse != expectedResponse {
-		t.Errorf("handler returned unexpected body: got %v want %v", actualResponse, expectedResponse)
+		t.Errorf("1: handler returned unexpected body: got %v want %v", actualResponse, expectedResponse)
+	}
+
+	req, err = http.NewRequest("GET", "/calculate?cidr=192.168.1.1/23", nil)
+	if err != nil {
+		t.Fatalf("Could not create request: %v", err)
+	}
+
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("2: handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	if actualResponse != expectedResponse {
+		t.Errorf("2: handler returned unexpected body: got %v want %v", actualResponse, expectedResponse)
 	}
 }
